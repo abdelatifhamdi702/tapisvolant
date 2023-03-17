8babd87f960a73485b204ca985e406741d33eddf
Mohamed-Ismail <sahli.med@outlook.com>
2023-03-17 11:02:02 +0100

    fix   next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const { i18n } = require('./next-i18next.config')
 module.exports = {
   env: {
     host: 'localhost',
-    port: '3000',
+    port: 4400,
     STRIPE_PUBLIC_API_KEY: ''
   },
   i18n,
